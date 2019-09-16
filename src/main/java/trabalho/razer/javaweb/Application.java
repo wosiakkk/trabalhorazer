e5b9e92a79a176996a72bf6647a668d10a2e14c7
Bruno Wosiak <31929337+wosiakkk@users.noreply.github.com>
2019-09-16 19:51:31 -0300

    first commit

---
+++ src/main/java/trabalho/razer/javaweb/Application.java
@@ -0,0 +1,25 @@
+package trabalho.razer.javaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class Application implements WebMvcConfigurer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+
+		registry.addViewController("/home").setViewName("/home");
+		registry.addViewController("/").setViewName("home");
+		registry.addViewController("/login").setViewName("login");
+		registry.addViewController("/hello").setViewName("hello");
+		
+	}
+
+}
