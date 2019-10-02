39ae5f4d0d641eb233c0251cdd71805b8df47564
Bruno Wosiak <31929337+wosiakkk@users.noreply.github.com>
2019-10-02 11:01:54 -0300

    Login controller. Criado controller para login para prevenir que o usuário já autenticado acesse a página de login novamente ou o index.

---
+++ src/main/java/trabalho/razer/javaweb/controller/LoginController.java
@@ -0,0 +1,26 @@
+package trabalho.razer.javaweb.controller;
+
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LoginController {
+	
+	@GetMapping(value = {"/login", "/index"})
+	private ModelAndView login() {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+		if (!(auth instanceof AnonymousAuthenticationToken)) {
+
+		    /* The user is logged in :) */
+		    return new ModelAndView("/home");
+		}
+		 return new ModelAndView("/login");
+	}
+	
+	
+}
